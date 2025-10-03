8a73b348b87ec47c8ce6e30a6bcd0ac0c2ad0fb5
Seele-Va <carlosalbertowu123@gmail.com>
2025-10-03 16:56:06 -0600

    logica de juego y animacion de dado

---
+++ Crazy_Risk/Core/Tarjeta.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyRisk.Core
+{
+    public class Tarjeta
+    {
+        public ETipoTarjeta Tipo { get; set; }
+        public string NombreTerritorio { get; set; }
+        public bool Utilizada { get; set; }
+        public Tarjeta(ETipoTarjeta tipo, string nombreTerritorio)
+        {
+            Tipo = tipo;
+            NombreTerritorio = nombreTerritorio;
+            Utilizada = false;
+        }
+    }
+}
